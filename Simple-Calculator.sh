29e631bb6b7fb62137f9ef3f829bffe651e4e6c1
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-12-29 12:52:32 +0000

    Update Simple-Calculator.sh

--- Simple-Calculator.sh
+++ Simple-Calculator.sh
@@ -10,3 +10,13 @@ echo "2. Subtraction"
 echo "3. Multiplication"
 echo "4. Division"
 read ch
+
+case $ch in
+  1)res=`echo $a + $b | bc`
+  ;;
+  2)res=`echo $a - $b | bc`
+  ;;
+  3)res=`echo $a \* $b | bc`
+  ;;
+  4)res=`echo "scale=2; $a / $b" | bc`
+  ;;
